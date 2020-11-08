8318b845c69d1a22ac68bad1b40fe5a74600bd69
Venky <venkatramanm@gmail.com>
2020-11-08 17:28:14 +0530

    UI Proto changes

--- src/main/java/in/succinct/mandi/controller/InventoriesController.java
+++ src/main/java/in/succinct/mandi/controller/InventoriesController.java
@@ -4,8 +4,10 @@ import com.venky.swf.controller.ModelController;
 import com.venky.swf.db.model.Model;
 import com.venky.swf.path.Path;
 import com.venky.swf.sql.Select.ResultFilter;
+import in.succinct.mandi.db.model.Attachment;
 import in.succinct.mandi.db.model.Facility;
 import in.succinct.mandi.db.model.Item;
+import in.succinct.mandi.db.model.Sku;
 import in.succinct.mandi.db.model.User;
 import in.succinct.plugins.ecommerce.db.model.inventory.Inventory;
 
@@ -22,15 +24,27 @@ public class InventoriesController extends ModelController<Inventory> {
     protected Map<Class<? extends Model>, List<String>> getIncludedModelFields() {
         Map<Class<? extends Model>, List<String>> map =  super.getIncludedModelFields();
         map.put(Facility.class, Arrays.asList("ID","NAME","DISTANCE","LAT","LNG"));
+        map.put(Attachment.class, Arrays.asList("ID"));
         return map;
     }
 
     @Override
+    protected Map<Class<? extends Model>, List<Class<? extends Model>>> getConsideredChildModels() {
+        Map<Class<? extends Model>, List<Class<? extends Model>>> cache =  super.getConsideredChildModels();
+        cache.get(Facility.class).add(Attachment.class);
+        cache.get(Sku.class).add(Attachment.class);
+        return cache;
+    }
+
+    @Override
     protected ResultFilter<Inventory> getFilter() {
+        int maxDistance = Math.max(20,getReflector().getJdbcTypeHelper().getTypeRef(Integer.class).getTypeConverter().
+                valueOf(getPath().getFormFields().get("MaxDistance")));
+
 
         return record ->
                 ((record.getFacility().getRawRecord().getAsProxy(Facility.class).isPublished() &&
-                        record.getFacility().getRawRecord().getAsProxy(Facility.class).getDistance() < 20 &&
+                        record.getFacility().getRawRecord().getAsProxy(Facility.class).getDistance() < maxDistance &&
                         record.getFacility().getCreatorUser().getRawRecord().getAsProxy(User.class).getBalanceOrderLineCount() > 0)
                         || record.getSku().getItem().getRawRecord().getAsProxy(Item.class).isHumBhiOnlineSubscriptionItem())
                         && (record.isInfinite() || record.getQuantity() > 0)
