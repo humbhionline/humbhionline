2d976edca26a11428e0b99f9b0936df99f2f299f
Venky <venkatramanm@gmail.com>
2021-02-25 14:12:13 +0530

    Minor fixes

--- src/main/java/in/succinct/mandi/integrations/courier/Wefast.java
+++ src/main/java/in/succinct/mandi/integrations/courier/Wefast.java
@@ -1,6 +1,7 @@
 package in.succinct.mandi.integrations.courier;
 
 import com.venky.core.util.Bucket;
+import com.venky.core.util.ObjectUtil;
 import com.venky.swf.integration.api.Call;
 import com.venky.swf.integration.api.InputFormat;
 import com.venky.swf.plugins.collab.db.model.participants.admin.Address;
@@ -12,8 +13,8 @@ import in.succinct.plugins.ecommerce.db.model.catalog.UnitOfMeasure;
 import in.succinct.plugins.ecommerce.db.model.catalog.UnitOfMeasureConversionTable;
 import in.succinct.plugins.ecommerce.db.model.order.OrderAddress;
 import in.succinct.plugins.ecommerce.db.model.order.OrderLine;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -22,9 +23,6 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Wefast {
-    public double getPrice(Order order){
-        return 0;
-    }
     public void createOrder(Order order){
         JSONObject orderJson = makeJson(order);
         Call<JSONObject> call = new Call<JSONObject>().url(Config.instance().getProperty("wefast.api.url") ,
@@ -40,36 +38,96 @@ public class Wefast {
 
     }
 
+
+    public double getPrice(Order order){
+        JSONObject orderJson = makeJson(order);
+        Call<JSONObject> call = new Call<JSONObject>().url(Config.instance().getProperty("wefast.api.url") ,
+                "/calculate-order").
+                header("X-DV-Auth-Token",Config.instance().getProperty("wefast.api.token")).
+                inputFormat(InputFormat.JSON).input(orderJson);
+
+        if (call.hasErrors()){
+            throw new RuntimeException(call.getError());
+        }
+
+        JSONObject response = call.getResponseAsJson();
+        Boolean success = (Boolean)response.get("is_successful");
+        if (success){
+            JSONObject jsonResponseOrder = (JSONObject) response.get("order");
+            return (Double)jsonResponseOrder.get("payment_amount");
+        }
+        return Double.POSITIVE_INFINITY;
+    }
+
     private JSONObject makeJson(Order order) {
         JSONObject obj = new JSONObject();
         Set<String> tags = new HashSet<>();
         Bucket weight = new Bucket();
+        JSONArray packages = new JSONArray();
+
         for (OrderLine line: order.getOrderLines()){
             StringTokenizer tok = new StringTokenizer(line.getInventory().getRawRecord().getAsProxy(Inventory.class).getTags());
             while (tok.hasMoreTokens()){
                 tags.add(tok.nextToken());
             }
-
         }
+        JSONObject p = new JSONObject();
+        p.put("ware_code",order.getFacility().getName());
+        p.put("description", "HumBhiOnline Order# " + order.getId());
+        p.put("items_count", order.getOrderLines().size());
+        packages.add(p);
+
         obj.put("matter",tags.toString());
         obj.put("total_weight", UnitOfMeasureConversionTable.convert(order.getWeight(), UnitOfMeasure.MEASURES_WEIGHT,order.getWeightUom(),
-                UnitOfMeasure.getMeasure(UnitOfMeasure.MEASURES_WEIGHT,"Kgs")));
+        UnitOfMeasure.getMeasure(UnitOfMeasure.MEASURES_WEIGHT,"Kgs")));
         obj.put("payment_method","cash");
         JSONArray points = new JSONArray();
         obj.put("points",points);
-        points.put(makeJson(order.getFacility(),order.getAddresses()));
+        points.addAll(makePoints(order));
+        ((JSONObject)points.get(0)).put("packages",packages);
+
+
         return obj;
     }
 
-    private List<JSONObject> makeJson(Facility shipFrom, List<OrderAddress> addresses) {
+    private List<JSONObject> makePoints(Order order) {
+        Facility shipFrom = order.getFacility();
+        List<OrderAddress> addresses = order.getAddresses();
         OrderAddress shipTo = addresses.stream().filter(a->a.getAddressType().equals(OrderAddress.ADDRESS_TYPE_SHIP_TO)).findFirst().get();
-        for (Address address : new Address[]{shipFrom,shipTo}){
 
+        JSONObject from = fillPoint(order,shipFrom) ;
+
+        JSONObject to = fillPoint(order,shipTo);
+
+        to.put("is_order_payment_here",true);
+        to.put("delivery_id",1);
+        to.put("name",shipTo.getFirstName() + " " + shipTo.getLastName());
+
+        return Arrays.asList(from,to);
+    }
+    private JSONObject fillPoint(Order order,Address address){
+        JSONObject point = new JSONObject();
+        StringBuilder buff = new StringBuilder();
+        if (!ObjectUtil.isVoid(address.getAddressLine4())){
+            buff.append(address.getAddressLine4());
+        }else if (!ObjectUtil.isVoid(address.getAddressLine3())){
+            buff.append(address.getAddressLine3());
+        }else if (!ObjectUtil.isVoid(address.getAddressLine2())){
+            buff.append(address.getAddressLine2());
+        }else if (!ObjectUtil.isVoid(address.getAddressLine1())){
+            buff.append(address.getAddressLine1());
         }
-        JSONObject p1 = new JSONObject();
-        StringBuilder address = new StringBuilder();
-        p1.put("address",shipFrom.getName() + "," + shipFrom.getAddressLine1());
+        buff.append(",").append(address.getCity().getName()).append(",").append(address.getState().getName());
 
-        return Arrays.asList(p1);
+        point.put("address",buff.toString());
+        JSONObject person = new JSONObject();
+        point.put("contact_person", person);
+        person.put("phone",address.getPhoneNumber());
+        point.put("client_order_id",order.getId());
+        point.put("latitude",address.getLat().doubleValue());
+        point.put("longitude",address.getLng().doubleValue());
+
+
+        return point;
     }
 }
