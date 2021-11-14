823d1c65a8adcdbbf7839cc6310c7cb6585c1bb4
Venky <venkatramanm@gmail.com>
2021-11-14 23:18:12 +0530

    Entering Delivery Address init

---
+++ src/main/java/in/succinct/mandi/controller/AddressesController.java
@@ -0,0 +1,24 @@
+package in.succinct.mandi.controller;
+
+import com.venky.swf.controller.VirtualModelController;
+import com.venky.swf.path.Path;
+import com.venky.swf.plugins.collab.extensions.beforesave.BeforeSaveAddress.LocationSetterTask;
+import com.venky.swf.views.View;
+import in.succinct.mandi.db.model.Address;
+
+import java.util.List;
+
+public class AddressesController extends VirtualModelController<Address> {
+    public AddressesController(Path path) {
+        super(path);
+    }
+    public View locate(){
+        List<Address> addressList = getIntegrationAdaptor().readRequest(getPath());
+
+        for (Address address : addressList){
+            new LocationSetterTask<>(address).setLatLng(false);
+        }
+        return getReturnIntegrationAdaptor().createResponse(getPath(),addressList);
+    }
+
+}
---
+++ src/main/java/in/succinct/mandi/db/model/Address.java
@@ -0,0 +1,9 @@
+package in.succinct.mandi.db.model;
+
+
+import com.venky.swf.db.annotations.column.IS_VIRTUAL;
+import com.venky.swf.db.model.Model;
+
+@IS_VIRTUAL
+public interface Address extends com.venky.swf.plugins.collab.db.model.participants.admin.Address, Model {
+}
